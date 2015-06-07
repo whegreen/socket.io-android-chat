ad5791259d04dc8e3a3925a6b8b937b5deb2d486
Walesa Danto <walesa.danto@mivo.com>
2015-06-07 17:45:06 +0700

    initial commit

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,6 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "http://chat.socket.io";
+//    public static final String CHAT_SERVER_URL = "http://chat.socket.io";
+    public static final String CHAT_SERVER_URL = "http://socketio-dev.elasticbeanstalk.com";
 }
